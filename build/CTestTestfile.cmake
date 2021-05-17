3c31bbee719ec61ef35ca30ab5cd6d25805144d5
Chad Samuelson <crsamuelson87@gmail.com>
2021-05-17 10:31:54 -0600

    updated new dist 2anch code

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/chad/pozyx/src
+# Build directory: /home/chad/pozyx/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
--- src/dist_2anch_1tag.py
+++ src/dist_2anch_1tag.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-#from pypozyx import PozyxSerial, get_first_pozyx_serial_port, POZYX_SUCCESS, SingleRegister, EulerAngles, Acceleration
 from pypozyx import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,37 +7,46 @@ import time
 
 from pypozyx.lib import Device
 
-class Tag2AnchorDistance():
-    def __init__(self,pozyx,remote_id,anchor_id):        
+class Tag2AnchorsDistance():
+    def __init__(self,pozyx,remote_id,anchor1_id,anchor2_id):        
         self.pozyx = pozyx
 
         # Clear all anchors connected to remote id tag
         self.pozyx.clearDevices(remote_id=remote_id)
         
         # Add anchor(s) to remote device
-        anchor1 = DeviceCoordinates(anchor_id,1,Coordinates(0,0,0))
+        anchor1 = DeviceCoordinates(anchor1_id,1,Coordinates(0,0,0))
+        anchor2 = DeviceCoordinates(anchor2_id,1,Coordinates(3000,0,0)) #offset anchors by 1.5 meters
         self.pozyx.addDevice(anchor1,remote_id)
+        self.pozyx.addDevice(anchor2,remote_id)
 
         # State Variables
         self.remote_id = remote_id
-        self.anchor_id = anchor_id
+        self.anchor1_id = anchor1_id
         self.anchor1 = anchor1
+        self.anchor2_id = anchor2_id
+        self.anchor2 = anchor2
         self.device_range = DeviceRange()
         self.prev_time = None
         self.max_rate = None
         self.max_dist = 0.
 
         # Variables for plotting
-        self.list_of_dist = []
+        self.list_of_dist1 = []
+        self.list_of_dist2 = []
         self.init_time = None
-        self.list_of_times = []
+        self.list_of_times1 = []
+        self.list_of_times2 = []
 
     def run(self):
-        self.device_range = DeviceRange()
+        self.device1_range = DeviceRange()
         status = self.pozyx.doRanging(
-            self.anchor_id, self.device_range, self.remote_id)
+            self.anchor1_id, self.device1_range, self.remote_id)
+        self.device2_range = DeviceRange()
+        status &= self.pozyx.doRanging(
+            self.anchor2_id, self.device2_range, self.remote_id)
         if self.init_time is None:
-            self.init_time = self.device_range.timestamp
+            self.init_time = self.device1_range.timestamp
         if status == POZYX_SUCCESS:
             # Update sampling rate if larger
             if self.prev_time is None:
@@ -50,11 +58,15 @@ class Tag2AnchorDistance():
                 elif curr_range_rate > self.max_rate:
                     self.max_rate = curr_range_rate
             # Append distance and correlated time measurements
-            print("Distance(m):",self.device_range.distance/1000)
-            if self.device_range.distance > self.max_dist:
-                self.max_dist = self.device_range.distance
-            self.list_of_times.append(self.device_range.timestamp-self.init_time)
-            self.list_of_dist.append(self.device_range.distance)
+            print('Distance(m) from: Anchor 1={}, Anchor 2={}'.format(self.device1_range.distance/1000,self.device2_range.distance/1000))
+            if self.device1_range.distance > self.max_dist:
+                self.max_dist = self.device1_range.distance
+            if self.device2_range.distance > self.max_dist:
+                self.max_dist = self.device2_range.distance
+            self.list_of_times1.append(self.device1_range.timestamp-self.init_time)
+            self.list_of_times2.append(self.device2_range.timestamp-self.init_time)
+            self.list_of_dist1.append(self.device1_range.distance)
+            self.list_of_dist2.append(self.device2_range.distance)
         else:
             error_code = SingleRegister()
             status = self.pozyx.getErrorCode(error_code)
@@ -65,17 +77,22 @@ class Tag2AnchorDistance():
                 print("ERROR Ranging, couldn't retrieve local error")
 
     def plot(self):
+        print()
         print("Plotting...")
-        distance_values = np.asarray(self.list_of_dist)
-        t = np.asarray(self.list_of_times)
+        distance_values1 = np.asarray(self.list_of_dist1)
+        distance_values2 = np.asarray(self.list_of_dist2)
+        t1 = np.asarray(self.list_of_times1)
+        t2 = np.asarray(self.list_of_times2)
 
         # Plot Results
         fig = plt.figure(1)
-        plt.plot(t/1000,distance_values/1000)
-        plt.title("1 Tag to 1 Anchor: Max Distance Test")
+        plt.plot(t1/1000,distance_values1/1000,label='Distance to Anchor 1')
+        plt.plot(t2/1000,distance_values2/1000,label='Distance to Anchor 2')
+        plt.title("1 Tag to 2 Anchors: Max Distance Test")
         plt.xlabel("Time (s)")
         plt.ylabel("Distance (m)")
         plt.text(1,1,'Max Sampling Rate: {}'.format(round(self.max_rate,2)))
+        plt.legend()
         plt.show()
 
         # Get and Print UWB Settings
@@ -106,8 +123,9 @@ if __name__ == "__main__":
         quit()
     
     # Device ID Config
-    remote_id = None #0x7607                 # remote tag device network ID
-    anchor1_id = 0x7612                  # anchor device network ID
+    remote_id = 0x7607            # remote tag device network ID
+    anchor1_id = 0x7612                 # anchor 1 device network ID
+    anchor2_id = 0x7653                 # anchor 2 device network ID
     
     pozyx = PozyxSerial(serial_port)
 
@@ -177,7 +195,7 @@ if __name__ == "__main__":
     #     quit()
     
     
-    t2a = Tag2AnchorDistance(pozyx,remote_id,anchor1_id)
+    t2a = Tag2AnchorsDistance(pozyx,remote_id,anchor1_id,anchor2_id)
     try:
         while True:
             t2a.run()
